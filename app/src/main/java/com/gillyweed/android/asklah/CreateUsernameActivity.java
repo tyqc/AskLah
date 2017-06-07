fefa41d7a26e7bb0cec8fdd74446e576cedb8c7f
Cheryl Toh <atlanticcod@hotmail.com>
2017-06-07 22:02:56 +0200

    Create Username Layout Link LoginActivity button with CreateUsernameActivity

---
+++ app/src/main/java/com/gillyweed/android/asklah/CreateUsernameActivity.java
@@ -0,0 +1,13 @@
+package com.gillyweed.android.asklah;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CreateUsernameActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_username);
+    }
+}
--- app/src/main/java/com/gillyweed/android/asklah/LoginActivity.java
+++ app/src/main/java/com/gillyweed/android/asklah/LoginActivity.java
@@ -1,7 +1,13 @@
 package com.gillyweed.android.asklah;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import static android.R.attr.start;
+import static com.gillyweed.android.asklah.R.string.login;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -9,5 +15,19 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        Button loginButton = (Button) findViewById(R.id.login_button);
+
+        // Set a clickListener on that view
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Create a new intent to open the {@link CreateUsernameActivity}
+                Intent loginIntent = new Intent(LoginActivity.this, CreateUsernameActivity.class);
+
+                // Start the new activity
+                startActivity(loginIntent);
+            }
+        });
     }
 }
