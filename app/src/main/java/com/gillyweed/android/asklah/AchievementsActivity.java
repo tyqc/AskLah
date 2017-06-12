8d35d5ef3b1d6faf896358060e75cc9318ae37a2
Cheryl Toh <atlanticcod@hotmail.com>
2017-06-12 21:54:08 +0200

    Attach Achievement Adapter

--- app/src/main/java/com/gillyweed/android/asklah/AchievementsActivity.java
+++ app/src/main/java/com/gillyweed/android/asklah/AchievementsActivity.java
@@ -7,6 +7,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutCompat;
 import android.view.MenuItem;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class AchievementsActivity extends AppCompatActivity {
 
@@ -14,6 +18,17 @@ public class AchievementsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_achievements);
+
+        // Construct the data source
+        ArrayList<Achievement> achievementArrayList = new ArrayList<Achievement>();
+        achievementArrayList.add(new Achievement("Question Ninja", true));
+
+        // Create the adapter to convert the array list to views
+        AchievementAdapter achievementAdapter = new AchievementAdapter(this, achievementArrayList);
+
+        // Attach the adapter to a list view
+        ListView achievementsListView = (ListView) findViewById(R.id.achievement_list_view);
+        achievementsListView.setAdapter(achievementAdapter);
 
         // Add back button onto action bar
         ActionBar actionBar = getSupportActionBar();
