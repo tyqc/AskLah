21f2d690e655aa5d0a65be6947eb6b525c3d34b6
Cheryl Toh <atlanticcod@hotmail.com>
2017-06-12 23:23:23 +0200

    Attach Adapter to AnnouncementActivity

--- app/src/main/java/com/gillyweed/android/asklah/AnnouncementsActivity.java
+++ app/src/main/java/com/gillyweed/android/asklah/AnnouncementsActivity.java
@@ -5,6 +5,9 @@ import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class AnnouncementsActivity extends AppCompatActivity {
 
@@ -16,6 +19,17 @@ public class AnnouncementsActivity extends AppCompatActivity {
         // Add back button onto action bar
         ActionBar actionBar = getSupportActionBar();
         actionBar.setDisplayHomeAsUpEnabled(true);
+
+        // Construct the data source
+        ArrayList<Announcement> announcementArrayList = new ArrayList<Announcement>();
+        announcementArrayList.add(new Announcement("megaNerd", "3 seconds ago", "Rapid Tapper"));
+
+        // Create the adapter to convert the arraylist to views
+        AnnouncementAdapter announcementAdapter = new AnnouncementAdapter(this, announcementArrayList);
+
+        // Attach the adapter to a listview
+        ListView announcementsListView = (ListView) findViewById(R.id.announcement_list_view);
+        announcementsListView.setAdapter(announcementAdapter);
     }
 
     @Override
