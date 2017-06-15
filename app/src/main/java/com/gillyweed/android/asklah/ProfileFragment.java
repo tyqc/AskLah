7216545b15585b7178d5531ffe8e8ac84483ec00
Cheryl Toh <atlanticcod@hotmail.com>
2017-06-15 22:06:41 +0200

    Do layout for profile fragment

--- app/src/main/java/com/gillyweed/android/asklah/ProfileFragment.java
+++ app/src/main/java/com/gillyweed/android/asklah/ProfileFragment.java
@@ -5,6 +5,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 public class ProfileFragment extends Fragment {
 
@@ -18,6 +19,9 @@ public class ProfileFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_profile, container, false);
+        TextView usernameTextView = (TextView) rootView.findViewById(R.id.username_text_view);
+        usernameTextView.setText("@kyholmes");
+        return rootView;
     }
 }
